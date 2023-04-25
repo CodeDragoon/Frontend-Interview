395574049d2b7d17a64307d8bc234626883323f8
Rishabh Nigam <rishabhnigam1@gmail.com>
2023-04-26 01:12:51 +0530

    GitHub links (#5)
    * LRU Cache Implementation
    
    * added js questions
    
    * edits to lru cache
    
    * rearranged components
    
    * route created
    
    * nested comments component
    
    * created about
    
    * add github links

--- src/common-components/layout/Layout.jsx
+++ src/common-components/layout/Layout.jsx
@@ -2,8 +2,29 @@ import React from "react";
 import { Link, Outlet } from "react-router-dom";
 import "./styles.css";
 import LabradorSvg from "../../assets/svgs/labradorSvg";
+import data from "../../assets/problems.json";
 
+const useLayout = () => {
+  const getGithubLink = () => {
+    const path = window.location.pathname;
+    const problems = data.problems;
+    for (let i = 0; i < problems.length; i++) {
+      let item = problems[i];
+      if (path.indexOf(item.route) >= 0) {
+        return item.github || "";
+      }
+    }
+
+    return "";
+  };
+  const handleCheckImplementation = () => {
+    const gh = getGithubLink();
+    window.open(gh);
+  };
+  return { handleCheckImplementation };
+};
 const Layout = () => {
+  const { handleCheckImplementation } = useLayout();
   return (
     <div>
       <div className="header">
@@ -22,6 +43,12 @@ const Layout = () => {
       <div className="outlet-container">
         <Outlet />
       </div>
+      <button
+        className={"implementation-button"}
+        onClick={handleCheckImplementation}
+      >
+        Check Implementation
+      </button>
     </div>
   );
 };
